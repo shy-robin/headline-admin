389c99ead03212a96bc521ed588a6fddb36978a9
Shallowbeing <load_more@163.com>
2021-08-06 09:29:02 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui' // 加载组件库
+import 'element-ui/lib/theme-chalk/index.css' // 加载组件库样式
+
+Vue.use(ElementUI) // 全局注册组件库
 
 Vue.config.productionTip = false
 
