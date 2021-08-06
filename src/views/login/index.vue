400dc50de5cfad1dfc2db323ea1570781ccd0a47
Shallowbeing <load_more@163.com>
2021-08-06 13:47:59 +0800

    feat: 添加登录表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,24 +2,24 @@
   <div class="login-container">
     <div class="login-form-wrapper">
       <div class="logo-head">明日头条</div>
-      <el-form ref="form" :model="user">
-          <el-form-item>
+      <el-form ref="loginForm" :model="user" :rules='formRules'>
+          <el-form-item prop='mobile'>
             <el-input
               v-model="user.mobile"
               placeholder='请输入手机号'
               prefix-icon='el-icon-phone-outline'
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop='code'>
             <el-input
               v-model="user.code"
               placeholder='请输入验证码'
               prefix-icon='el-icon-edit-outline'
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop='isAgree'>
             <el-checkbox
-              v-model="checked"
+              v-model="user.isAgree"
             >我已阅读并同意用户协议和隐私条款</el-checkbox>
           </el-form-item>
           <el-form-item>
@@ -44,34 +44,60 @@ export default {
     return {
       user: {
         mobile: '', // 手机号
-        code: '' // 验证码
+        code: '', // 验证码
+        isAgree: false // 是否同意协议
       },
-      checked: false, // 是否勾选条款
-      isLoading: false // 是否登录中
+      isLoading: false, // 是否登录中
+      formRules: {
+        mobile: [
+          { required: true, message: '手机号不为空', trigger: 'blur' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式不正确', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '验证码不为空', trigger: 'blur' },
+          { pattern: /^\d{6}$/, message: '验证码格式不正确', trigger: 'change' }
+        ],
+        isAgree: [
+          {
+            validator(rule, value, callback) {
+              if (value) {
+                callback() // 校验成功
+              }
+              callback(new Error('请同意协议')) // 校验失败，返回错误信息
+            },
+            trigger: 'change'
+          }
+        ]
+      }
     }
   },
   methods: {
-    async onLogin() {
-      // 1.获取表单数据
-      const user = this.user
-      // 2.表单验证
-      // 3.发送请求，处理响应
+    onLogin() {
+      // 点击按钮表单验证
+      this.$refs.loginForm.validate(valid => {
+        // 验证失败，返回
+        if (!valid) return false
+        // 验证成功，进行登录请求
+        this.login()
+      })
+    },
+    async login() {
       this.isLoading = true // 登录中 loading...
 
+      // 发送请求，处理响应
       try {
         const res = await request({
           method: 'POST',
           url: '/mp/v1_0/authorizations',
-          data: user // 请求body
+          data: this.user // 请求body
         })
-        this.isLoading = false
         this.$msgSuccess('登录成功！')
         console.log(res)
       } catch (ex) { // 捕获错误
-        this.isLoading = false
         this.$msgError('登录失败！')
         console.log(ex)
       }
+      this.isLoading = false // 关闭 loading
     }
   }
 }
