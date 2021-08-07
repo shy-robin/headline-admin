3a208dc88e05f93f4bb33728d4593988550eeae8
Shallowbeing <load_more@163.com>
2021-08-07 11:45:39 +0800

    style: 内容管理 - 底部筛选结果卡片

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="article-container">
-    <el-card class="box-card">
+    <el-card class="filter-card">
       <div slot="header" class="clearfix">
         <el-breadcrumb separator-class="el-icon-arrow-right">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
@@ -40,6 +40,37 @@
         </el-form-item>
       </el-form>
     </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>根据筛选条件共查询到 46777 条结果</span>
+      </div>
+      <el-table
+        :data="tableData"
+        border
+        size='medium'
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        class="pagination"
+        background
+        layout="prev, pager, next"
+        :total="100">
+      </el-pagination>
+    </el-card>
   </div>
 </template>
 
@@ -57,12 +88,41 @@ export default {
         type: [],
         resource: '',
         desc: ''
-      }
+      },
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.article-container {
+  .filter-card {
+    margin-bottom: 20px;
+  }
+  .pagination {
+    padding-top: 20px;
+  }
+}
 </style>
