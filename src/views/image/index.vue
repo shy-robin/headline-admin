908306d8fa36526e893ba026d24b23d23919332d
Shallowbeing <load_more@163.com>
2021-08-09 10:02:57 +0800

    feat: 素材管理 - 添加素材上传功能

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -7,14 +7,22 @@
           <el-breadcrumb-item>素材管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
-      <el-radio-group
-        v-model="isCollect"
-        size="mini"
-        @change='onLabelChange'
-      >
-        <el-radio-button :label="false">全部</el-radio-button>
-        <el-radio-button :label="true">收藏</el-radio-button>
-      </el-radio-group>
+      <div style='display:flex;justify-content:space-between;'>
+        <el-radio-group
+          v-model="isCollect"
+          size="mini"
+          @change='onLabelChange'
+        >
+          <el-radio-button :label="false">全部</el-radio-button>
+          <el-radio-button :label="true">收藏</el-radio-button>
+        </el-radio-group>
+        <el-button
+          type="success"
+          round
+          size="mini"
+          @click="dialogVisible = true"
+        >上传素材</el-button>
+      </div>
       <el-row :gutter="20">
         <el-col
           v-for="(item, index) in imageList" :key="index"
@@ -38,6 +46,26 @@
         :total="totalCount">
       </el-pagination>
     </el-card>
+    <el-dialog
+      title="上传素材"
+      :visible.sync="dialogVisible"
+      :append-to-body="true"
+    >
+      <el-upload
+        drag
+        v-if="dialogVisible"
+        action="http://api-toutiao-web.itheima.net/mp/v1_0/user/images"
+        :headers="uploadHeaders"
+        name="image"
+        :on-success="onUploadSuccess"
+        :on-error="onUploadError"
+        multiple
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传图片格式文件，且图片不能太大！！！</div>
+      </el-upload>
+    </el-dialog>
   </div>
 </template>
 
@@ -47,12 +75,18 @@ import { getImageList } from 'api/image.js'
 export default {
   name: 'ImageIndex',
   data() {
+    const token = window.localStorage.getItem('user')
+
     return {
       isCollect: false, // 默认显示全部图片
       perPage: 10, // 默认每页请求 10 张
       imageList: [], // 图片列表
       totalCount: 0, // 图片总数
-      page: 1 // 当前第几页，默认为第 1 页
+      page: 1, // 当前第几页，默认为第 1 页
+      dialogVisible: false, // 展示上传图片窗口
+      uploadHeaders: { // 图片上传请求头
+        Authorization: `Bearer ${token}`
+      }
     }
   },
   created() {
@@ -76,6 +110,16 @@ export default {
     onLabelChange(isCollect) {
       this.isCollect = isCollect
       this.loadImageList(this.page)
+    },
+    onUploadSuccess() {
+      this.dialogVisible = false // 关闭上传窗口
+      this.loadImageList(1) // 重新显示图片列表
+      this.$msgSuccess('图片上传成功！') // 弹出成功消息提示
+    },
+    onUploadError() {
+      this.dialogVisible = false
+      this.loadImageList(1)
+      this.$msgError('图片尺寸过大！')
     }
   }
 }
@@ -89,5 +133,8 @@ export default {
   .pagination {
     padding-top: 10px;
   }
+}
+::v-deep .el-dialog {
+  width: 400px;
 }
 </style>
