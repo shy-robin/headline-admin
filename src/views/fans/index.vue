80c2ad18bc9201a50286c7698fcd34f12dddd82a
Shallowbeing <load_more@163.com>
2021-08-10 20:05:35 +0800

    style: 粉丝管理 - 页面布局

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    粉丝管理
+  <div class="fans-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>粉丝管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-tabs
+        v-model="activeTab"
+        type="card"
+      >
+        <el-tab-pane label="粉丝列表" name="list">list</el-tab-pane>
+        <el-tab-pane label="粉丝画像" name="chart">figure</el-tab-pane>
+      </el-tabs>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FansIndex'
+  name: 'FansIndex',
+  data() {
+    return {
+      activeTab: 'list'
+    }
+  }
 }
 </script>
 
