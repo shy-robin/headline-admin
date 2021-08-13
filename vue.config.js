d4404f2d789bfbe88ae9c5aac24e2f4eb5e98841
Shallowbeing <load_more@163.com>
2021-08-13 10:23:43 +0800

    feat: 打包优化 - 使用CDN地址忽略打包一些体积较大的第三方包，加快打包速度

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,13 @@ module.exports = {
         'utils': path.resolve(__dirname, 'src/utils'),
         'api': path.resolve(__dirname, 'src/api'),
       }
+    },
+    // 告诉 webpack 使用 script 的全局变量加载，而不是使用 node_modules 的包
+    externals: {
+      // 【包名】: 【script 中暴露的全局变量名（在 CDN 文件上找）】
+      'vue': 'Vue',
+      'element-ui': 'ELEMENT',
+      'echarts': 'echarts'
     }
   }
 }
